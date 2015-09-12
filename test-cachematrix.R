a099a12be0a645f976d600e6ee9462206f29c84d
Leo Lincourt <leolincourt@gmail.com>
2015-09-12 10:50:01 -0400

    Add unit test

---
+++ test-cachematrix.R
@@ -0,0 +1,22 @@
+## Tests functioning of makeCacheMatrix() and CacheSolve()
+test.cachematrix = function(test_mat){
+# @test_mat: an invertible matrix
+# Outputs results of test
+  
+  temp = makeCacheMatrix(test_mat)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  exec.time.uncached = Sys.time() - start.time
+  message("Non-cached execution time is ", exec.time, "seconds.")
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  exec.time.cached = Sys.time() - start.time
+  message("Cached execution time is ", exec.time, "seconds.")
+  
+  if (exec.time.uncached > exec.time.cached)
+    message ("Cached lookup was shorter. All is well with the world.")
+  else
+    message("Hrrm. Something's not right. The cached time should be shorter.")
+}
